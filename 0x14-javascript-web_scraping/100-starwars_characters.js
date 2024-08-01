e53635585adb096a685125d035df99efc137b4f8
StellarTeshiBlank <blankteshi079@gmail.com>
2024-08-01 02:32:22 +0100

    Javascript Web Scraping

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,32 +1,17 @@
 #!/usr/bin/node
 const request = require('request');
-const url = `http://swapi.dev/api/films/${process.argv[2]}/`;
+const url = 'http://swapi.co/api/films/' + process.argv[2];
 
-function fetchUrl(url) {
-  return new Promise((resolve, reject) => {
-    request(url, (err, response, body) => {
-      if (err) {
-        reject(err);
-      } else if (response.statusCode !== 200) {
-        reject(new Error(`Failed to fetch data. Status code: ${response.statusCode}`));
-      } else {
-        resolve(body);
-      }
-    });
-  });
-}
-
-async function main() {
-  try {
-    const body = await fetchUrl(url);
+request(url, function (err, response, body) {
+  if (err == null) {
     const resp = JSON.parse(body);
     const characters = resp.characters;
-
-    // Output the number of characters
-    console.log(`Number of characters: ${characters.length}`);
-  } catch (err) {
-    console.error('Error fetching film:', err);
+    for (let i = 0; i < characters.length; i++) {
+      request(characters[i], function (err, response, body) {
+        if (err == null) {
+          console.log(JSON.parse(body).name);
+        }
+      });
+    }
   }
-}
-
-main();
+});
