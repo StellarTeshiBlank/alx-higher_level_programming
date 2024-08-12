dd9dab6125d9dc4bf872967738bc442be80b949c
StellarTeshiBlank <blankteshi079@gmail.com>
2024-08-12 17:45:57 +0100

    Python Class

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,9 +1,16 @@
 #!/usr/bin/python3
+"""
+This module defines a class Square based on 0-Square
+"""
+
+
 class Square:
-    """Class Square that defines a square object
+    """
+    Class Square that defines a square object
     """
     def __init__(self, size):
-        """Initialize method that stores the size of the square
+        """
+        Initialize method that stores the size of the square
 
         Args:
             param1 (int): size of the square
--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,9 +1,16 @@
 #!/usr/bin/python3
+"""
+This Module defines a Class Square by its size
+"""
+
+
 class Square:
-    """ A class that defines a square by its size
+    """ 
+    A class that defines a square by its size
     """
     def __init__(self, size=0):
-        """ Method to initialize the square object
+        """ 
+        Method to initialize the square object
         """
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
