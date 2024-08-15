73b1436fbd0120830d1ea4fb187468f6fb01f015
StellarTeshiBlank <blankteshi079@gmail.com>
2024-08-15 14:18:34 +0100

    More Classes(Corrected)

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -3,6 +3,7 @@
 This module has a class that defines a Rectangle
 """
 
+
 class Rectangle:
     """
     Class that defines a rectangle
@@ -108,7 +109,8 @@ class Rectangle:
         """
         if self.width == 0 or self.height == 0:
             return ""
-        return "\n".join(str(self.print_symbol) * self.width for _ in range(self.height))
+        return "\n".join(str(self.print_symbol) *
+                         self.width for _ in range(self.height))
 
     def __repr__(self):
         """
