69fbfa25631b0949be1ff85a72ee9131fa2b918d
StellarTeshiBlank <blankteshi079@gmail.com>
2024-09-25 23:50:06 +0100

    Python Networks

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -5,20 +5,23 @@ Finds a peak inside a list
 
 
 def find_peak(list_of_integers):
-    if not list_of_integers:
+    if list_of_integers == []:
         return None
 
     length = len(list_of_integers)
-    mid = length // 2
+    mid = int(length / 2)
     li = list_of_integers
 
+    if mid - 1 < 0 and mid + 1 >= length:
+        return li[mid]
+    elif mid - 1 < 0:
+        return li[mid] if li[mid] > li[mid + 1] else li[mid + 1]
+    elif mid + 1 >= length:
+        return li[mid] if li[mid] > li[mid - 1] else li[mid - 1]
 
-    if (mid == 0 or li[mid] >= li[mid - 1]) and (mid == length - 1 or li[mid] >= li[mid + 1]):
+    if li[mid - 1] < li[mid] > li[mid + 1]:
         return li[mid]
 
-
-    if mid < length - 1 and li[mid] < li[mid + 1]:
-        return find_peak(li[mid + 1:])
-
-
+    if li[mid + 1] > li[mid - 1]:
+        return find_peak(li[mid:])
     return find_peak(li[:mid])
